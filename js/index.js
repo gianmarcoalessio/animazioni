01ba0f8a3b4d358e3ab9654143680cb0880987b3
gianmarioale <giammi.alessio@gmail.com>
2021-04-08 09:57:44 +0200

    date e regolazione del frameRate

--- js/index.js
+++ js/index.js
@@ -17,17 +17,29 @@ function windowResized() {
 function setup() {
     init();
     createCanvas(o.CW, o.CH).parent("canvas");
-    frameRate(10) 
 }
 function draw() {
     background("whitesmoke");
     //simg.draw(0,10,10,200,200);
-    spriteBW.draw(10,10,100,100);
-    spriteWB.draw(10,150,100,100);
+    spriteBW.draw(10, 10, 100, 100);
+    spriteWB.draw(10, 150, 100, 100);
 }
-function preload(){
-    simg=new Spriteimg("./img/pedine.png",4,8)
-    spriteBW= new Sprite(simg,[6,7,8,9,10,11,12,13,14],2000)//passiamo la sequenza prendendo le immagini dello sprite dalla 6-14, in 2 secondi
-    spriteWB= new Sprite(simg,[14,13,12,11,10,9,8,7,6],4000)//passiamo la sequenza prendendo le immagini dello sprite dalla 14-6, in 4 secondi
+function preload() {
+    simg = new Spriteimg("./img/pedine.png", 4, 8)
+    spriteBW = new Sprite(simg, [6, 7, 8, 9, 10, 11, 12, 13, 14], 500)//passiamo la sequenza prendendo le immagini dello sprite dalla 6-14, in 2 secondi
+    spriteWB = new Sprite(simg, [14, 13, 12, 11, 10, 9, 8, 7, 6], 500)//passiamo la sequenza prendendo le immagini dello sprite dalla 14-6, in 4 secondi
+}
+function keyPressed() {
+    switch (key) {
+        case "r":
+            spriteBW.reset();
+            spriteWB.reset();
+            break;
+        case "s":
+            spriteBW.start();
+            spriteWB.start();
+            break;
+
+    }
 }
 
