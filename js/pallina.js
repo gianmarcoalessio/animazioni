4a7cfee2c0d55e20999b925ba3f426bb0fa3e7d7
gianmarioale <giammi.alessio@gmail.com>
2021-04-08 11:36:07 +0200

    pedine non sincronizzati ma stesso frame

--- js/pallina.js
+++ js/pallina.js
@@ -1,5 +1,6 @@
 const COEFF=1
-
+//matter.js libreria da andare a vedere per lo studio delle animazioni
+//three.js, babylon.js per animazioni 3D si basano su webgl che a sua volta si basa su opengl, noi con p5.js ci siamo basati su CANVAS
 class Pallina{
     constructor(){
         //vogliamo sapere dove si trova la pallina e la sua velocità
@@ -9,6 +10,9 @@ class Pallina{
         this.vy=random(-3,0);
         this.ay=0.1;
         this.color=color(random(255),random(255),random(255))//colori casuali per pallina
+        
+        this.spriteBW = new Sprite(simg, [6, 7, 8, 9, 10, 11, 12, 13, 14], 313)//passiamo la sequenza prendendo le immagini dello sprite dalla 6-14, in 2 secondi
+        this.spriteBW.start(true)
     }
     update(){
         this.x+=this.vx
@@ -23,7 +27,8 @@ class Pallina{
         }
     }
     draw(){
-        fill(this.color)
-        circle(this.x,this.y,20)
+        //fill(this.color)
+        //circle(this.x,this.y,40)
+        this.spriteBW.draw(this.x,this.y,40,40)
     }
 }
