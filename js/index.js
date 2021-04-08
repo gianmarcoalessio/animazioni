92f3321d8189bae9f423c5e489eb3f5e0437ec41
gianmarioale <giammi.alessio@gmail.com>
2021-04-08 09:32:01 +0200

    sprite prima animazione incontrallabile

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-var simg;
+var simg;// sprite image
+var spriteBW;//sprite da black a white
+var spriteWB;//sprite da white a black
 
 var o = {
     CW: 0,
@@ -14,12 +16,18 @@ function windowResized() {
 }
 function setup() {
     init();
-    createCanvas(o.CW, o.CH).parent("canvas"); 
+    createCanvas(o.CW, o.CH).parent("canvas");
+    frameRate(10) 
 }
 function draw() {
     background("whitesmoke");
-    simg.draw(0,10,10,200,200);
+    //simg.draw(0,10,10,200,200);
+    spriteBW.draw(10,10,100,100);
+    spriteWB.draw(10,150,100,100);
 }
 function preload(){
     simg=new Spriteimg("./img/pedine.png",4,8)
+    spriteBW= new Sprite(simg,[6,7,8,9,10,11,12,13,14],2000)//passiamo la sequenza prendendo le immagini dello sprite dalla 6-14, in 2 secondi
+    spriteWB= new Sprite(simg,[14,13,12,11,10,9,8,7,6],4000)//passiamo la sequenza prendendo le immagini dello sprite dalla 14-6, in 4 secondi
 }
+
--- js/sprite.js
+++ js/sprite.js
@@ -3,5 +3,25 @@ class Sprite {
         this.simg=simg;
         this.frames=frames;
         this.time=time;
+        this.reset();//cosí il constructor chiama sempre la funzione reset
     }
+    start(){
+
+    }
+    draw(x,y,w,h){
+        //draw della frame
+        if(this.index>=0 && this.index<this.frames.length){
+            this.simg.draw(this.frames[this.index],x,y,w,h)//questo draw è il draw della classe spriteimg con l'index come primo elemento
+        }
+        this.update()
+    }
+    //cambia il contatore quando lo ha appena disegnato
+    update(){
+        if(this.index<(this.frames.length-1)) this.index++;
+    }
+    reset(){
+        this.index=0 //qual è la frame attiva, quindi parto dalla zero
+
+    }
+    
 }
